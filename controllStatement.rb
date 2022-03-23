663f95adcc2ccfc664b249ca17124f6dbf2f6d3b
Yuvraj Singh Dodiya <jarvuy7@gmail.com>
2022-03-23 12:41:16 +0530

    First Commit

---
+++ controllStatement.rb
@@ -0,0 +1,30 @@
+  # if condition
+  a = true
+if a = true
+    puts "a is true"
+else 
+    put "a is False" 
+end
+
+b = 5
+if b==1
+    puts "b is 1"
+elsif b==2
+    puts "b is 2"
+else
+    puts "b is something else"
+end
+
+# loops in Ruby
+
+#while loop
+x = 1
+while x <=5
+     puts "Hello Ruby"
+      x = x + 1
+end
+
+#for loop
+for i in 1..10
+    puts i
+end
