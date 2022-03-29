6fd57385fe3fda471a146bd7880b5e53109fabee
Yuvraj Singh Dodiya <jarvuy7@gmail.com>
2022-03-29 17:34:20 +0530

    Feedback changes on 29/03/2022

--- module_util.rb
+++ module_util.rb
@@ -6,26 +6,26 @@ def exempt_good?(str)
   gives = false
   for i in list_of_exempt_item
     if str.include?(i)
-    gives = true
+      gives = true
     end
-   end
-   gives
+  end
+  gives
 end
 
   # to check if the entered item is in valid forma
-  def is_valid_entry?(str)
-    str
-    gives= true
-    split_1 = str.split(" ")
-    split_2 = str.split(" at ")
-    quatity = split_1[0].to_i
-    price = split_2[1].to_f
+def is_valid_entry?(str)
+  str
+  gives= true
+  split_1 = str.split(" ")
+  split_2 = str.split(" at ")
+  quatity = split_1[0].to_i
+  price = split_2[1].to_f
 
-    if quatity==0 || price == 0
-      gives  = false
-    end  #end of if condition
-    gives
- end  # end of is_valid_entry? method
+  if quatity==0 || price == 0
+    gives  = false
+  end  #end of if condition
+  gives
+end  # end of is_valid_entry? method
 
 def create_new_bill
   item = Array.new
@@ -33,15 +33,15 @@ def create_new_bill
   puts "Enter 'Done' When all items are entered"
   x = 0
   while true do
-     item[x] = gets
-     x += 1
+    item[x] = gets
+    x += 1
    # Using Break Statement
-     break if item[x-1].include?("Done")
-     if is_valid_entry?(item[x-1])==false
-       puts "Invalid Entry 'Fatal'"
-       create_new_bill
+    break if item[x-1].include?("Done")
+    if is_valid_entry?(item[x-1])==false
+      puts "Invalid Entry 'Fatal'"
+      create_new_bill
      end
-   end  #end of while loop
+  end  #end of while loop
   item
  end   # end of create new bill
 end  # end module
--- tax_problem.rb
+++ tax_problem.rb
@@ -26,39 +26,34 @@ class Billing
     @total_tax_amount
   end
 
-  def calculate_tax
-    for i in @purchase_list
-      str = i
-      split_1= str.split(" ")
-      split_2 = str.split(" at ")
-      quatity = split_1[0].to_i
-      item  = split_2[0]
-      price = split_2[1].to_f
+def calculate_tax
+  for item in @purchase_list
+    split_1= item.split(" ")
+    split_2 = item.split(" at ")
+    quatity = split_1[0].to_i
+    item  = split_2[0]
+    price = split_2[1].to_f
 
-      if split_2[0].include?("imported")
-        sales_tax_amount = (price * sales_tax) / 100
-        sales_tax_amount  = sales_tax_amount.round(2)
-        total_tax_amount = total_tax_amount.to_f + sales_tax_amount.to_f
-      end
+    if split_2[0].include?("imported")
+      sales_tax_amount = ((price * sales_tax) / 100 ).round(2)
+      total_tax_amount = total_tax_amount.to_f + sales_tax_amount.to_f
+    end
 
-      basic_tax_amount = (price * basic_tax) / 100
-      basic_tax_amount = basic_tax_amount.round(2)
-      final_price = price + sales_tax_amount.to_f + basic_tax_amount.to_f
-      total_tax_amount = total_tax_amount.to_f + basic_tax_amount.to_f
+    basic_tax_amount = ((price * basic_tax) / 100 ).round(2)
+    final_price = price + sales_tax_amount.to_f + basic_tax_amount.to_f
+    total_tax_amount = total_tax_amount.to_f + basic_tax_amount.to_f
 
-      if TaxModule.exempt_good?(item)
-        final_price = price
-        total_tax_amount = total_tax_amount.to_f - basic_tax_amount.to_f
-      end
+    if TaxModule.exempt_good?(item)
+      final_price = price
+      total_tax_amount = total_tax_amount.to_f - basic_tax_amount.to_f
+    end
 
-      total_amount = total_amount.to_f  + final_price.to_f
-      print split_2[0]
-      print " at "
-      puts final_price.to_f.round(2)
+    total_amount = total_amount.to_f  + final_price.to_f
+    puts"#{split_2[0]} at #{final_price.to_f.round(2)}" 
 
-    end  # end of for loop
-    puts "total tax: #{total_tax_amount.round(2)}"
-    puts "Total : #{total_amount.round(2)}"
+   end  # end of for loop
+   puts "total tax: #{total_tax_amount.round(2)}"
+   puts "Total : #{total_amount.round(2)}"
   end  #calculateTax function end here
 end  # end of class here
 
