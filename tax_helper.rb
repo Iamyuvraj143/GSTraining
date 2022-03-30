aa4a8c136b4d129dc772ca0a21a5faf85056151b
Yuvraj Singh Dodiya <jarvuy7@gmail.com>
2022-03-30 16:15:16 +0530

    feedback changes on 30/03/2022

---
+++ tax_helper.rb
@@ -0,0 +1,34 @@
+module TaxHelper
+
+  LIST_OF_EXEMPT_ITEMS = %w(food medicin chocolates pills book books chocolate )
+
+# to check is item exempt from tax
+  def exempt_good?(str)
+    item  = str.split(" ")
+    exempted_item = item & LIST_OF_EXEMPT_ITEMS
+    exempted_item.empty? ? false : true
+  end
+
+  # to check if the entered item is in valid forma
+  def is_valid_entry?(str)
+    quantity = item.split(" ")[0].to_i
+    price = item.split(" at ")[1].to_f
+    quatity == 0 || price == 0 ? false : true
+  end   # end of is_valid_entry? method
+
+  def create_new_bill
+    items = []
+    puts "Enter the item purchase \n(example:1 book at 12.49 [quantity + item name and detail + at + price])"
+    puts "Enter 'Done' When all items are entered"
+    while true do
+      input = gets
+      break if input.include?("Done")
+      if is_valid_entry?(input) == false
+        puts "Invalid Entry 'Fatal'"
+        create_new_bill
+      end
+      items.append(input)
+    end  #end of while loop
+    items
+  end # end of create new bill
+end  # end module
--- tax_problem.rb
+++ tax_problem.rb
@@ -1,64 +1,47 @@
-require_relative "module_util.rb"
-include TaxModule
+require_relative "tax_helper.rb"
+include TaxHelper
 
 class Billing
+
   def initialize(list_of_purchase)
     @purchase_list = list_of_purchase
   end
 
-  def sales_tax
-    @sales_tax = 5
-  end
+  SALES_TAX = 5
+  BASIC_TAX = 10
+    
+  @purchase_list
 
-  def basic_tax
-    @basic_tax = 10
-  end
+  def calculate_tax
+    for item in @purchase_list
+      split_1= item.split(" ")
+      split_2 = item.split(" at ")
+      quatity = split_1[0].to_i
+      item  = split_2[0]
+      price = split_2[1].to_f
 
-  def purchase_list
-    @purchase_list
-  end
+      if split_2[0].include?("imported")
+        sales_tax_amount = ((price * SALES_TAX) / 100 ).round(2)
+        total_tax_amount = total_tax_amount.to_f + sales_tax_amount.to_f
+      end
 
-  def total
-    @total_amount
-  end
-
-  def total_tax_amount
-    @total_tax_amount
-  end
-
-def calculate_tax
-  for item in @purchase_list
-    split_1= item.split(" ")
-    split_2 = item.split(" at ")
-    quatity = split_1[0].to_i
-    item  = split_2[0]
-    price = split_2[1].to_f
-
-    if split_2[0].include?("imported")
-      sales_tax_amount = ((price * sales_tax) / 100 ).round(2)
-      total_tax_amount = total_tax_amount.to_f + sales_tax_amount.to_f
-    end
-
-    basic_tax_amount = ((price * basic_tax) / 100 ).round(2)
-    final_price = price + sales_tax_amount.to_f + basic_tax_amount.to_f
-    total_tax_amount = total_tax_amount.to_f + basic_tax_amount.to_f
-
-    if TaxModule.exempt_good?(item)
-      final_price = price
-      total_tax_amount = total_tax_amount.to_f - basic_tax_amount.to_f
-    end
-
-    total_amount = total_amount.to_f  + final_price.to_f
-    puts"#{split_2[0]} at #{final_price.to_f.round(2)}" 
-
-   end  # end of for loop
-   puts "total tax: #{total_tax_amount.round(2)}"
-   puts "Total : #{total_amount.round(2)}"
+      if exempt_good?(item) == false 
+        basic_tax_amount = ((price * BASIC_TAX) / 100 ).round(2)
+        total_tax_amount = total_tax_amount.to_f + basic_tax_amount.to_f
+      end
+    
+      final_price = price + sales_tax_amount.to_f + basic_tax_amount.to_f
+      total_amount = total_amount.to_f  + final_price.to_f
+      puts"#{split_2[0]} at #{final_price.to_f.round(2)}"
+      sales_tax_amount = 0
+      basic_tax_amount = 0
+    end  # end of for loop
+    puts "total tax: #{total_tax_amount.to_f.round(2)}"
+    puts "Total : #{total_amount.round(2)}"
   end  #calculateTax function end here
 end  # end of class here
 
 # program  launch from here
-list_of_purchase = TaxModule.create_new_bill
-list_of_purchase.pop
-b = Billing.new(list_of_purchase)
-b.calculate_tax
+list_of_purchase = create_new_bill
+bill = Billing.new(list_of_purchase)
+bill.calculate_tax
